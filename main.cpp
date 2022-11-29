4e35b09c720b8257114c2bbab469d399db51e789
Aurélio <100155495+LeloJNM@users.noreply.github.com>
2022-11-29 17:32:55 -0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -20,21 +20,33 @@ float s1, s2; //raizes da equação
 
 cout << "Digite o valor de R: ";
 cin >> R;
-cout << "Digite a unidade de R: ";
+cout << "Digite a unidade de R(dado em potencia de 10): ";
 cin >> unidade;
 R = R * pow(10, unidade);
 
 cout << "Digite o valor de L: ";
 cin >> L;
-cout << "Digite a unidade de L: ";
+cout << "Digite a unidade de L(dado em potencia de 10): ";
 cin >> unidade;
 L = L * pow(10, unidade);
 
 cout << "Digite o valor de C: ";
 cin >> C;
-cout << "Digite a unidade de C: ";
+cout << "Digite a unidade de C(dado em potencia de 10): ";
 cin >> unidade;
 C = C * pow(10, unidade);
+
+cout << "digite o valor de vC: ";
+cin >> vC;
+cout <<"digite a unidade de vC(dado em potencia de 10): ";
+cin >> unidade;
+vC = vC * pow(10, unidade);
+
+cout << "digite o valor de iL: ";
+cin >> iL;
+cout <<"digite a unidade de iL(dado em potencia de 10): ";
+cin >> unidade;
+iL = iL * pow(10, unidade);
 
 sigma = 1/(2*R*C);
 omegaZero = 1/(sqrt(L*C));
