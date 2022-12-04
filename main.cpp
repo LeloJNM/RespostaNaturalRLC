027d01eecac08891840094a1088ebbe8c62de800
Aurélio <aureliojr2002@gmail.com>
2022-12-04 11:39:21 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -19,6 +19,9 @@ float s1, s2; //raizes da equação
 float iC; //corrente no capacitor
 float vL; //tensão no indutor
 float a1, a2;
+float t; //Tempo
+
+// adquirindo valores  e as condiçoes iniciais
 
 cout << "Digite o valor de R: ";
 cin >> R;
@@ -50,31 +53,33 @@ cout <<"digite a unidade de iL(dado em potencia de 10): ";
 cin >> unidade;
 iL = iL * pow(10, unidade);
 
+//calculando sigma, omega zero e omega d
+
 sigma = 1/(2*R*C); 
 omegaZero = 1/(sqrt(L*C));
 omegaD = sqrt((omegaZero*omegaZero) - (sigma*sigma));
 
-ic = -iL; // sabe-se que a corrente no capacitor é o inverso da corrente no indutor
+iC = -iL; // sabe-se que a corrente no capacitor é o inverso da corrente no indutor
 
 //a1 + a2 = tensao inicial
 
-
+//calculando os casos de superamortecido para sigma > omegazero, subamortecido quando sigma < omegazero e criticamente amortecido quando sigma = omegazero
 
 if (sigma>omegaZero){
-    cout << "Aurélio José e João"; << endl;
+    cout << "Aurélio José e João" << endl;
     cout << "Este circuito é superamortecido" << endl;
     s1 = -sigma + sqrt((sigma*sigma) - (omegaZero*omegaZero));
     s2 = -sigma - sqrt((sigma*sigma) - (omegaZero*omegaZero));
     cout << "s1 = " << s1 << endl;
     cout << "s2 = " << s2 << endl;
-    a2 = ((s1*vc)-vl) / (s1-s2);
-    a1 = vc - a2;
+    a2 = ((s1*vC)-vL) / (s1-s2);
+    a1 = vC - a2;
     cout << "a1 = " << a1 << endl;
     cout << "a2 = " << a2 << endl;
     cout << "Resposta: " << vC*exp(s1*t) + iL*exp(s2*t) << endl;
 }
 else if (sigma<omegaZero){
-    cout << "Aurélio José e João"; << endl;
+    cout << "Aurélio José e João" << endl;
     cout << "Resposta exponencial crescente subamortecido" << endl;
     s1 = -sigma + sqrt((sigma*sigma) - (omegaZero*omegaZero));
     s2 = -sigma - sqrt((sigma*sigma) - (omegaZero*omegaZero));
@@ -84,7 +89,7 @@ else if (sigma<omegaZero){
     cout << "Resposta: " << vC*exp(s1*t) + iL*exp(s2*t) << endl;
 }
 else if (sigma==omegaZero){
-    cout << "Aurélio José e João"; << endl;
+    cout << "Aurélio José e João" << endl;
     cout << "Resposta exponencial criticamente amortecido" << endl;
     s1 = -sigma + sqrt((sigma*sigma) - (omegaZero*omegaZero));
     s2 = -sigma - sqrt((sigma*sigma) - (omegaZero*omegaZero));
